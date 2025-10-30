df24eb82d21687862bde573f1aa36207105231c3
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-10-30 16:07:14 +0100

    finalizzazione rotta show per singolo film + reviews per ogni movie

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -16,7 +16,32 @@ function index(req, res) {
 
 function show(req, res) {
 
-    console.log("richiesta rotta show");
+    // recuperiamo l'id 
+    const id = req.params.id
+
+    // prima query di rireca di singolo movie
+    const movieSql = 'SELECT * FROM movies WHERE id = ?';
+
+    // Prepariamo la query per le reviews con join e where
+    const reviewsSql =  'SELECT * FROM reviews WHERE movie_id = ?';
+
+    // Eseguiamo la prima query per il movie
+    connection.query(movieSql, [id], (err, movieResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (movieResults.length === 0) return res.status(404).json({ error: 'Movie not found' });
+
+        // Recuperiamo il film (movie)
+        const movie = movieResults[0];
+
+        // eseguiamo la seconda query per le reviews
+        connection.query(reviewsSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+
+            // Aggiungiamo i le reviews dei movies
+            movie.reviews = reviewsResults;
+            res.json(movie);
+        });
+    });
 
 }
 
