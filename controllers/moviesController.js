2ab94648e6dacefd184bda57c28deecd5cc9799a
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-10-30 15:51:32 +0100

    finalizzazione rotta index lista movies

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -3,8 +3,14 @@ const connection = require('../data/db');
 
 function index(req, res) {
 
-    console.log("richiesta rotta index");
+    // prepariamo la query
+    const sql = 'SELECT * FROM movies';
 
+    // eseguiamo la query
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        res.json(results);
+    });
 }
 
 
